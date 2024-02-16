5f42e4206c5de553133045ca822e05e9eb464835
Omar Fahmy <omar101@hotmail.com>
2024-02-16 20:39:14 +0200

    formatting document

--- js/app.js
+++ js/app.js
@@ -1,82 +1,12 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
- * Define Global Variables
- 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
-//global variables
 const navBar = document.querySelector('.navbar__menu');
 const navBarList = document.querySelector('#navbar__list');
 const sections = document.querySelectorAll('section');
 
-function buildNavigation(){
-    sections.forEach((section, index) => {
-        const listItem = document.createElement('li');
-        const sectionLink = createSectionLink(index);
-        listItem.appendChild(sectionLink);
-        navBarList.appendChild(listItem);
-    })
-}
-function createSectionLink(index){
+function createSectionLink(index) {
     const sectionLink = document.createElement('a');
-    sectionLink.textContent = "Section " + (index+1) ;
-    sectionLink.href = "#section" + (index+1) ;
+    sectionLink.textContent = "Section " + (index + 1);
+    sectionLink.href = "#section" + (index + 1);
     sectionLink.classList.add('section-link')
     sectionLink.style.color = 'orange';
     sectionLink.style.textDecoration = 'none';
@@ -84,11 +14,7 @@ function createSectionLink(index){
     return sectionLink
 };
 
-document.addEventListener('DOMContentLoaded', buildNavigation);
-
-
-
-function buildNavigation(){
+function buildNavigation() {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             const id = entry.target.getAttribute('id');
@@ -107,7 +33,7 @@ function buildNavigation(){
         listItem.appendChild(sectionLink);
         navBarList.appendChild(listItem);
 
-        observer.observe(document.querySelector(`#section${index+1}`));
+        observer.observe(document.querySelector(`#section${index + 1}`));
     })
 };
 
