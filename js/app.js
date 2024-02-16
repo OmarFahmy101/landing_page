7f2f67d0dc39a0398974d130fe6d8d91abc09e0d
Omar Fahmy <omar101@hotmail.com>
2024-02-16 20:28:01 +0200

    added detecting section in the view port

--- js/app.js
+++ js/app.js
@@ -63,26 +63,52 @@
 //global variables
 const navBar = document.querySelector('.navbar__menu');
 const navBarList = document.querySelector('#navbar__list');
+const sections = document.querySelectorAll('section');
 
-function updateNavbar() {
-
-    const sections = document.querySelectorAll('section');
-
-
-    while (navBarList.firstChild) {
-        navBarList.removeChild(navBarList.firstChild);
-    }
-
-    sections.forEach(section => {
-        const sectionName = section.id;
-        const newLinkElement = document.createElement('a');
-        newLinkElement.href = '#' + sectionName;
-        newLinkElement.textContent = sectionName;
-        navBarList.appendChild(newLinkElement);
-    });
+function buildNavigation(){
+    sections.forEach((section, index) => {
+        const listItem = document.createElement('li');
+        const sectionLink = createSectionLink(index);
+        listItem.appendChild(sectionLink);
+        navBarList.appendChild(listItem);
+    })
 }
+function createSectionLink(index){
+    const sectionLink = document.createElement('a');
+    sectionLink.textContent = "Section " + (index+1) ;
+    sectionLink.href = "#section" + (index+1) ;
+    sectionLink.classList.add('section-link')
+    sectionLink.style.color = 'orange';
+    sectionLink.style.textDecoration = 'none';
+    sectionLink.style.fontFamily = 'verdana';
+    return sectionLink
+};
+
+document.addEventListener('DOMContentLoaded', buildNavigation);
 
 
-document.addEventListener('DOMContentLoaded', updateNavbar);
 
+function buildNavigation(){
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            const id = entry.target.getAttribute('id');
+            const navLink = document.querySelector(`nav a[href="#${id}"]`);
+            if (entry.isIntersecting) {
+                navLink.style.color = 'red';
+            } else {
+                navLink.style.color = 'orange';
+            }
+        });
+    }, { threshold: 0.7 });
 
+    sections.forEach((section, index) => {
+        const listItem = document.createElement('li');
+        const sectionLink = createSectionLink(index);
+        listItem.appendChild(sectionLink);
+        navBarList.appendChild(listItem);
+
+        observer.observe(document.querySelector(`#section${index+1}`));
+    })
+};
+
+document.addEventListener('DOMContentLoaded', buildNavigation);
